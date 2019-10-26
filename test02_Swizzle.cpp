4da422c8557b95209bd8a4be5cb81a35b717f0b0
kiorisyshen <kiorisyshen@gmail.com>
2019-10-27 01:46:46 +0900

    Update swizzle

--- test02_Swizzle.cpp
+++ test02_Swizzle.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 
-template <class TT, int A, int B>
+template <class TT, int... Indexes>
 class swizzle {
-    float v[2];
+    // here sizeof...() is a function to query the number of parameter pack
+    float v[sizeof...(Indexes)];
 
    public:
     TT &operator=(const TT &rhs) {
-        v[A] = rhs.x;
-        v[B] = rhs.y;
+        int indexes[] = {Indexes...};  // unpack
+        for (int i = 0; i < sizeof...(Indexes); i++) {
+            v[indexes[i]] = rhs[i];  // access pack element
+        }
         return *(TT *)this;
     }
-    TT operator*(const TT &rhs) {
-        float result[2];
-        result[0] = v[0] * rhs[0];
-        result[1] = v[1] * rhs[1];
-        return TT(result[0], result[1]);
+    TT operator*(const TT &rhs) const {
+        int indexes[] = {Indexes...};
+        float result[sizeof...(Indexes)];
+        for (size_t i = 0; i < sizeof...(Indexes); i++)
+            result[i] = v[indexes[i]] * rhs[i];
+        return TT(result[Indexes]...);
+    }
+    TT operator*(float &m) const {
+        int indexes[] = {Indexes...};
+        float result[sizeof...(Indexes)];
+        for (size_t i = 0; i < sizeof...(Indexes); i++)
+            result[i] = v[indexes[i]] * m;
+        return TT(result[Indexes]...);
     }
     operator TT() const {
-        return TT(v[A], v[B]);
+        // unpack
+        return TT(v[Indexes]...);
     }
 };
 
@@ -41,18 +53,57 @@ struct Vector2Type {
     operator const float *() const {
         return static_cast<const float *>(data);
     };
-
-    friend std::ostream &operator<<(std::ostream &os, const Vector2Type &vec);
 };
 
-std::ostream &operator<<(std::ostream &os, const Vector2Type &vec) {
-    os << "(" << vec[0] << ", " << vec[1] << ")";
+// Vector3d
+struct Vector3Type {
+    union {
+        float data[3];
+        struct {
+            float x, y, z;
+        };
+        swizzle<Vector2Type, 0, 1> xy;
+        swizzle<Vector2Type, 1, 0> yx;
+        swizzle<Vector2Type, 0, 2> xz;
+        swizzle<Vector2Type, 2, 0> zx;
+        swizzle<Vector2Type, 1, 2> yz;
+        swizzle<Vector2Type, 2, 1> zy;
+        swizzle<Vector3Type, 0, 1, 2> xyz;
+        swizzle<Vector3Type, 1, 0, 2> yxz;
+        swizzle<Vector3Type, 0, 2, 1> xzy;
+        swizzle<Vector3Type, 2, 0, 1> zxy;
+        swizzle<Vector3Type, 1, 2, 0> yzx;
+        swizzle<Vector3Type, 2, 1, 0> zyx;
+    };
+    Vector3Type(){};
+    Vector3Type(const float &_v)
+        : x(_v), y(_v), z(_v){};
+    Vector3Type(const float &_x, const float &_y, const float &_z)
+        : x(_x), y(_y), z(_z){};
+    operator float *() {
+        return data;
+    };
+    operator const float *() const {
+        return static_cast<const float *>(data);
+    };
+};
+
+std::ostream &operator<<(std::ostream &os, const Vector3Type &vec) {
+    os << "(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")";
     return os;
 }
 
+typedef Vector3Type Vec3D;
+
+void testFunc(const Vec3D &qtn) {
+    Vec3D result = qtn.xyz * 2.0;
+    std::cout << result << std::endl;
+}
+
 int main(int argc, char **argv) {
-    Vector2Type vec1(1.0, 2.0);
-    Vector2Type vec2(1.0, 2.0);
-    Vector2Type result = vec1.xy * 2.0;
+    testFunc({1.0, 2.0, 3.0});
+
+    Vec3D vec(3.0, 2.0, 1.0);
+    Vec3D result = vec.xyz * 2.0;
     std::cout << result << std::endl;
 }
