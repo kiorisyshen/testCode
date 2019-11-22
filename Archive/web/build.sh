9b41cbc27246aa52200f6d1c1809f0f419681d75
SijieShen <sijie.shen@dji.com>
2019-11-22 18:36:58 +0900

    Reorganize & clean cmake

--- Archive/web/build.sh
+++ Archive/web/build.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
+set -e
 
-EMSDK_GIT="../emsdk"
-
-if [ -d $EMSDK_GIT ] 
-then
-    echo "update emsdk"
-    $EMSDK_GIT/emsdk update-tags
-else
-    echo "clone from https://github.com/emscripten-core/emsdk.git "
-    git clone https://github.com/emscripten-core/emsdk.git $EMSDK_GIT
-    $EMSDK_GIT/emsdk install latest
-fi
-
-$EMSDK_GIT/emsdk activate latest
-
-rm -rf build        && \
-mkdir build         && \
-cd build            && \
-cmake ../WebAssembly/.
+mkdir -p build
+rm -rf build/*
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../../../wasm/cmake/Emscripten.cmake ../WebAssembly/.
+cmake --build . -v
+cd -
