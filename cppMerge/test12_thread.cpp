3a9c53d241add3a89305a743856c2d47065a1fbb
kiorisyshen <kiorisyshen@gmail.com>
2020-03-16 12:56:44 +0900

    code for thread lock

--- cppMerge/test12_thread.cpp
+++ cppMerge/test12_thread.cpp
@@ -14,10 +14,16 @@ void waits() {
     std::unique_lock<std::mutex> lk(cv_m);
     std::cerr << "Start waits()... \n";
 
-    // cv.wait(lk);
+    cv.wait(lk);
     for (int k = 0; k < 4; ++k) {
+        std::srand(std::time(nullptr));  // use current time as seed for random generator
+        int random_time = 1 + std::rand() % 100;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(random_time));
+        ++i;
         std::cerr << "i=" << i << "\n";
     }
+    std::cerr << std::endl;
 }
 
 void signals() {
@@ -28,20 +34,20 @@ void signals() {
     }
     cv.notify_all();
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    {
-        std::lock_guard<std::mutex> lk(cv_m);
-        i = 1;
-        std::cerr << "Notifying again...\n";
-    }
-    cv.notify_all();
+    // {
+    //     std::lock_guard<std::mutex> lk(cv_m);
+    //     i = 1;
+    //     std::cerr << "Notifying again...\n";
+    // }
+    // cv.notify_all();
 }
 
 int main() {
-    std::thread t1(waits), t2(waits), t3(waits);
+    std::thread t1(waits), t2(waits), t3(waits), t4(signals);
     t1.join();
     t2.join();
     t3.join();
-    // t4.join();
+    t4.join();
 }
